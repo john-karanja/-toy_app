90466330ce8b6de87a7367288aec02361aecd443
john-karanja <arakanj@gmail.com>
2015-06-24 09:44:48 +0300

    added validations to microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :user 
 	validates :content, length: { maximum: 140 }
+	validates :content, presence: true
+	validates :user, presence: true
 end
